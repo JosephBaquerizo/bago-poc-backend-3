31ebd9af63ccf3ec5fe30ac6cf9f7dc8120b920c
Joseph Baquerizo <jbaquerizof@gmail.com>
2023-05-16 09:15:29 -0500

    inicializando proyecto

---
+++ src/main/java/com/poc/bagobackend/BagoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poc.bagobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/bagobackend/BagoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.bagobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
