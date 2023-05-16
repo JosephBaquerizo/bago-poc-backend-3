b697a509719a5a00d725b0d963b0986e08dd1a58
Joseph Baquerizo <jbaquerizof@gmail.com>
2023-05-16 10:41:15 -0500

    restaurando proyecto

---
+++ src/main/java/com/poc/bagobackend/entities/Representative.java
@@ -0,0 +1,15 @@
+package com.poc.bagobackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Representative {
+
+    private String name;
+    private String image;
+
+}
---
+++ src/main/java/com/poc/bagobackend/services/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.poc.bagobackend.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poc.bagobackend.entities.Employee;
+
+import com.poc.bagobackend.entities.EmployeeBirthday;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    public List<Employee> getEmployees() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("/app/employees.json");
+        Employee[] employees = mapper.readValue(file, Employee[].class);
+        return Arrays.asList(employees);
+    }
+
+    public List<EmployeeBirthday> getEmployeesBirthdays() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("/app/birthdays.json");
+        EmployeeBirthday[] employees = mapper.readValue(file, EmployeeBirthday[].class);
+        return Arrays.asList(employees);
+    }
+
+}
