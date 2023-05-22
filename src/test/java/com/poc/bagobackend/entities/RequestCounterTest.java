0b22fc133debd4e7b6ba0aab4c32f67571028fec
Joseph Baquerizo <jbaquerizof@gmail.com>
2023-05-22 14:56:31 -0500

    agregando tests

--- src/test/java/com/poc/bagobackend/entities/RequestCounterTest.java
+++ src/test/java/com/poc/bagobackend/entities/RequestCounterTest.java
@@ -1,26 +1,19 @@
 package com.poc.bagobackend.entities;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestCounterTest {
 
-    RequestCounter requestCounter;
-
-    @BeforeEach
-    void setUp() {
-        requestCounter = new RequestCounter();
-    }
+    private RequestCounter requestCounter;
 
     @Test
-    void testNombreEmployee() {
-        long expected = 1;
+    public void testIncrementRequestCount() {
+        RequestCounter requestCounter = new RequestCounter();
         requestCounter.incrementRequestCount();
-        long actual = requestCounter.getRequestCount();
-        assertEquals(expected, actual);
-        assertNotSame(2, actual);
+        requestCounter.incrementRequestCount();
+        assertEquals(2, requestCounter.getRequestCount());
     }
 
 }
