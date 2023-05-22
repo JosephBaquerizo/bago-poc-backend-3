2be91eee85b75f6f9e2fc17087ac695d576094b8
Joseph Baquerizo <jbaquerizof@gmail.com>
2023-05-22 12:15:29 -0500

    ruta de archivos

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 RUN mvn clean package -DskipTests
 FROM openjdk:11-jre-slim
 COPY --from=build /app/target/*.jar /app/app.jar
-COPY src/main/resources/birthdays.json /app/birthdays.json
-COPY src/main/resources/employees.json /app/employees.json
+#COPY src/main/resources/birthdays.json /app/birthdays.json
+#COPY src/main/resources/employees.json /app/employees.json
 EXPOSE 9090
 CMD ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/poc/bagobackend/services/EmployeeService.java
+++ src/main/java/com/poc/bagobackend/services/EmployeeService.java
@@ -19,14 +19,14 @@ public class EmployeeService {
     public List<Employee> getEmployees() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper = new ObjectMapper();
-        File file = new File("/app/employees.json");
+        File file = new File("src/main/resources/employees.json");
         Employee[] employees = mapper.readValue(file, Employee[].class);
         return Arrays.asList(employees);
     }
 
     public List<EmployeeBirthday> getEmployeesBirthdays() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        File file = new File("/app/birthdays.json");
+        File file = new File("src/main/resources/birthdays.json");
         EmployeeBirthday[] employees = mapper.readValue(file, EmployeeBirthday[].class);
         return Arrays.asList(employees);
     }
