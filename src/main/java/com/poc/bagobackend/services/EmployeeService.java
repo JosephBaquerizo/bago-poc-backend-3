3078d91defdbdd26fb1e63859128a3f297cd904e
Joseph Baquerizo <jbaquerizof@gmail.com>
2023-05-23 15:19:12 -0500

    ruta de archivos

--- src/main/java/com/poc/bagobackend/services/EmployeeService.java
+++ src/main/java/com/poc/bagobackend/services/EmployeeService.java
@@ -18,14 +18,14 @@ public class EmployeeService {
 
     public List<Employee> getEmployees() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        File file = new File("src/main/resources/employees.json");
+        File file = new File("/app/employees.json");
         Employee[] employees = mapper.readValue(file, Employee[].class);
         return Arrays.asList(employees);
     }
 
     public List<EmployeeBirthday> getEmployeesBirthdays() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        File file = new File("src/main/resources/birthdays.json");
+        File file = new File("/app/birthdays.json");
         EmployeeBirthday[] employees = mapper.readValue(file, EmployeeBirthday[].class);
         return Arrays.asList(employees);
     }
